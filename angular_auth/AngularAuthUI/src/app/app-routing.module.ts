fe85f9463d745823f08e6edd77000c1ee8b86370
zeeshan <zeeshan2211997@gmail.com>
2022-10-19 13:43:03 +0530

    redirect URL added

--- angular_auth/AngularAuthUI/src/app/app-routing.module.ts
+++ angular_auth/AngularAuthUI/src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
     path:'login',
     component:LoginComponent
   },
